33cfd32f9af396c909fa71c1dc69e9366e27f5fe
shanuka-umesh <shanuka@qbitum.com>
2025-01-29 16:49:17 +0530

    Created print method and method to create squares

--- tree-pattern/src/main/java/org/example/Main.java
+++ tree-pattern/src/main/java/org/example/Main.java
@@ -5,8 +5,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+
+
         char[][] canvas = createCanvas(10,10);
 
+        square(canvas,2,2);
+
+        printCanvas(canvas);
 
 
     }
@@ -25,10 +30,31 @@ public class Main {
         return canvas;
     }
 
-    public static void printCanvas()
+    public static void printCanvas(char[][] canvas)
     {
 
+      //  System.out.println(canvas[1]);
 
+        for (int i = 0; i < canvas.length; i++)
+        {
+            for (int j = 0; j< canvas[i].length; j++)
+            {
+                System.out.print(canvas[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static void square(char[][] canvas , int startPositionX , int startPositionY)
+    {
+        for(int i =startPositionX ; i<startPositionX+3 ; i++ )
+        {
+            for(int j=startPositionY ; j<startPositionY+3 ; j++)
+            {
+                canvas[i][j]='*';
+            }
+        }
     }
 
 

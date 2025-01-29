68f260908f8891e660ca7a6c9042fe455a3a76a0
shanuka-umesh <shanuka@qbitum.com>
2025-01-29 16:13:12 +0530

    Created methods to create a canvas

--- tree-pattern/src/main/java/org/example/Main.java
+++ tree-pattern/src/main/java/org/example/Main.java
@@ -4,14 +4,32 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        char[][] canvas = createCanvas(10,10);
+
+
+
     }
+
+    public static  char[][] createCanvas(int height, int width)
+    {
+        char[][] canvas = new char[height][width];
+
+        for(int i=0; i<height; i++)
+        {
+            for(int j=0; j<width ; j++)
+            {
+                canvas[i][j]= ' ';
+            }
+        }
+        return canvas;
+    }
+
+    public static void printCanvas()
+    {
+
+
+    }
+
+
 }

0eb26f962d6f2581dddbc43ae640eebd2d4a03b4
shanuka-umesh <shanuka@qbitum.com>
2025-01-31 11:29:49 +0530

    updated

--- tree-pattern/src/main/java/org/example/Main.java
+++ tree-pattern/src/main/java/org/example/Main.java
@@ -3,16 +3,19 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static int canvasHeight=20;
+    static int canvasWidth=20;
+
     public static void main(String[] args) {
 
+        char[][] canvas = createCanvas(canvasHeight,canvasWidth);
 
+         Createsquare(canvas,2,5,10);
 
-        char[][] canvas = createCanvas(10,10);
-
-        square(canvas,2,2);
+        CreateTriangle(canvas,5,5,4);
 
         printCanvas(canvas);
-
 
     }
 
@@ -33,29 +36,63 @@ public class Main {
     public static void printCanvas(char[][] canvas)
     {
 
-      //  System.out.println(canvas[1]);
+      //  System.out.println(canvas.length);
 
         for (int i = 0; i < canvas.length; i++)
         {
             for (int j = 0; j< canvas[i].length; j++)
             {
-                System.out.print(canvas[i][j] + " ");
+                if(i==0||j==0||i==canvas.length-1||j==canvas[i].length-1)
+                {
+                    System.out.print("` ");
+                }
+                else {
+                    System.out.print(canvas[i][j] + " ");
+                }
+
             }
             System.out.println("");
         }
 
     }
 
-    public static void square(char[][] canvas , int startPositionX , int startPositionY)
+    public static void Createsquare(char[][] canvas , int startPositionX , int startPositionY,int size)
     {
-        for(int i =startPositionX ; i<startPositionX+3 ; i++ )
+        if(canvasHeight>startPositionX+size && canvasWidth > startPositionY+size )
         {
-            for(int j=startPositionY ; j<startPositionY+3 ; j++)
+            for(int i =startPositionX ; i<startPositionX+size ; i++ )
             {
-                canvas[i][j]='*';
+                for(int j=startPositionY ; j<startPositionY+size ; j++)
+                {
+                    canvas[i][j]='*';
+                }
+            }
+        }
+        else
+        {
+            System.out.println("\nShape must be placed in canvas. Please place the shape inside the canvas");
+            System.exit(0);
+        }
+
+    }
+
+    public static void CreateTriangle(char[][] canvas,int startPositionX, int startPositionY, int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int spaces = size - i - 1; // Leading spaces for centering
+            int stars = 2 * i + 1; // Number of stars in current row
+
+            for (int j = 0; j < spaces; j++)
+            {
+                canvas[startPositionX + i][startPositionY + j] = ' ';
+            }
+
+            for (int j = 0; j < stars; j++)
+            {
+                canvas[startPositionX + i][startPositionY + spaces + j] = '*';
             }
         }
     }
-
 
 }

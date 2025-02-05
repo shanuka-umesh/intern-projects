118c029dbba0109be0a1ea9535521ae3dac582eb
shanuka-umesh <shanuka@qbitum.com>
2025-02-05 13:53:50 +0530

    Updated leaf class

--- Tree-pattern-v2/src/main/java/org/example/Elements.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-class Elements
-{
-
-
-}
--- Tree-pattern-v2/src/main/java/org/example/Tree.java
+++ Tree-pattern-v2/src/main/java/org/example/Tree.java
@@ -1,6 +1,24 @@
 package org.example;
 
-public class Tree
-{
+import javax.swing.*;
 
+public class Tree extends JFrame {
+
+    private Leaf canvas;
+    private int leafCount=100;
+
+    public Tree() {
+        super("Tree pattern version 2");
+        canvas = new Leaf(leafCount);
+
+        add(canvas);
+
+        setSize(700, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public void showTree() {
+        setVisible(true);
+    }
 }

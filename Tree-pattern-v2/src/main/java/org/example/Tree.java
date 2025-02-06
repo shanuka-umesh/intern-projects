a525b44072ca9ea5bc9049f234d7fcfe311dd9e1
shanuka-umesh <shanuka@qbitum.com>
2025-02-06 23:30:43 +0530

    Updated

--- Tree-pattern-v2/src/main/java/org/example/Tree.java
+++ Tree-pattern-v2/src/main/java/org/example/Tree.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class Tree extends JFrame {
 
-    private int roundleafCount = 200;
-    private int overleafCount = 150;
+    private int roundleafCount = 400;
+    private int overleafCount = 300;
 
     private Color[] leafColors = {
             new Color(61, 238, 61),
@@ -26,14 +26,15 @@ public class Tree extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        addTrunk();
         addLeaves();
+        addTrunk();
+
     }
 
     private void addTrunk()
     {
-        Trunk trunk = new Trunk(80, 400);
-        trunk.setBounds(250, 450, 200, 400);
+        Trunk trunk = new Trunk(100, 800);
+        trunk.setBounds(300, 350, 80, 400);
         add(trunk);
     }
 
@@ -49,17 +50,16 @@ public class Tree extends JFrame {
         {
             int xPosition = (int) (Math.random() * getWidth());
             int yPosition = (int) (Math.random() * getHeight() / 2);
-            int width = (int) (Math.random() * 20) + 10;
+            int width = (int) (Math.random() * 20) + 20;
             int height = width * 2;
 
             Color leafColor = leafColors[(int) (Math.random() * leafColors.length)];
 
             OverlLeaf leaf = new OverlLeaf(xPosition, yPosition, width, height, leafColor);
-            leaf.setBounds(0, 0, 700, 700);
+            leaf.setBounds(20, 20, 600, 600);
             add(leaf);
         }
     }
-
 
 
     private void addRoundLeaves()
@@ -72,7 +72,7 @@ public class Tree extends JFrame {
             Color leafColor = leafColors[(int) (Math.random() * leafColors.length)];
 
             RoundLeaf leaf = new RoundLeaf(xPosition, yPosition, diameter, leafColor);
-            leaf.setBounds(0, 0, 700, 700);
+            leaf.setBounds(20, 20, 600, 600);
             add(leaf);
         }
     }
